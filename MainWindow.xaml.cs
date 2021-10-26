e6c0910a6326c2c559e23142926d30e56d0c1f21
paul.kenning <kenningpaul@googlemail.com>
2021-10-26 17:16:27 +0200

    API Request hinzugefügt

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,111 +1,148 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.IO;
+using System.Net;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Urlaubsplaner
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
+
+        public JObject hollidays;
+
+        public struct Feiertag
+        {
+            public DateTime datum;
+            public string hinweis;
+        }
+
+        private List<Vacation> vacationList = new List<Vacation>();
+
         public MainWindow()
         {
             InitializeComponent();
-        }
-        public class Urlaub
-        {
-            public DateTime StartDatum { get; set; }
-            public DateTime EndDatum { get; set; }
-            public float Stunden { get; set; }
-            public bool Genommen { get; set; }
-        }
+            //sets the Datagrid Item Source
+            DGUrlaub.ItemsSource = vacationList;
 
-        public List<Urlaub> urlaubsliste = new List<Urlaub>();
+            hollidays = RequestApi();
+            IsHollyday(DateTime.Now);
+        }
+        public class Vacation
+        {
+            public DateTime StartDate { get; set; }
+            public DateTime EndDate { get; set; }
+            public float Hours { get; set; }
+            public bool Taken { get; set; }
+        }
 
         private void UrlaubEintagen(object sender, RoutedEventArgs e)
         {
-
-            DGUrlaub.ItemsSource = urlaubsliste;
-            urlaubsliste.Add(UrlaubErstellen());
-            DGUrlaub.Items.Refresh();
+            //Adds a new vacation object to the list
+            Vacation tVaction = InstantiateHollyday();
+            if (tVaction != null)
+            {
+                vacationList.Add(tVaction);
+                //Updates the Datagrid
+                DGUrlaub.Items.Refresh();
+            }
         }
 
-        private Urlaub UrlaubErstellen()
+        private Vacation InstantiateHollyday()
         {
-            DateTime tStartDatum, tEndDatum;
+            DateTime tStartDate, tEndDate;
             try
             {
-                tStartDatum = DPStartDatum.SelectedDate.Value;
-                tEndDatum = DPEndDatum.SelectedDate.Value;
+                tStartDate = DPStartDatum.SelectedDate.Value;
+                tEndDate = DPEndDatum.SelectedDate.Value;
             }
-            catch (Exception e)
+            catch (Exception)
             {
                 MessageBox.Show("Bitte ein gültiges Datum angeben");
-                throw;
+                return null;
             }
 
-            float tStunden = CalcStunden(tStartDatum,tEndDatum);
-            bool tGenommen = false;
+            float tHours = CalcStunden(tStartDate, tEndDate);
+            bool tTaken = false;
 
-            Urlaub urlaub = new Urlaub()
+            Vacation urlaub = new Vacation()
             {
-                StartDatum = tStartDatum,
-                EndDatum = tEndDatum,
-                Stunden = tStunden,
-                Genommen = tGenommen
+                StartDate = tStartDate,
+                EndDate = tEndDate,
+                Hours = tHours,
+                Taken = tTaken
             };
             return urlaub;
         }
 
-        private float CalcStunden(DateTime Start, DateTime Ende)
+        private float CalcStunden(DateTime Start, DateTime End)
         {
-            float stunden = 0;
-            int tage;
+            float lHours = 0;
+            int lDays;
 
-            tage = (Ende - Start).Days +1;
+            //calculate the days from end to start
+            lDays = (End - Start).Days + 1;
 
-            for (int i = 0; i < tage; i++)
+            //loops through each day between Start and End and summed up the hours.
+            for (int i = 0; i < lDays; i++)
             {
-                if (IsFeiertagOrWeekend(Start.AddDays(i)))
+                if (IsHolidayOrWeekend(Start.AddDays(i)))
                 {
-                    stunden += 0;
+                    lHours += 0;
                 }
                 else
                 {
                     if (Start.AddDays(i).DayOfWeek != DayOfWeek.Friday)
                     {
-                        stunden += 8.75f;
+                        lHours += 8.75f;
                     }
                     else
                     {
-                        stunden += 5;
+                        lHours += 5;
                     }
                 }
             }
-            return stunden;
+            return lHours;
         }
 
-        private bool IsFeiertagOrWeekend(DateTime tDay)
+        private bool IsHolidayOrWeekend(DateTime tDay)
         {
-            if (tDay.DayOfWeek == DayOfWeek.Saturday || tDay.DayOfWeek == DayOfWeek.Sunday)
+            //Checks, if the given day is either a holiday or a day in the weekend.
+            if (tDay.DayOfWeek == DayOfWeek.Saturday || tDay.DayOfWeek == DayOfWeek.Sunday || IsHollyday(tDay))
             {
                 return true;
             }
             else
             {
                 return false;
+            }
+        }
+
+        private bool IsHollyday(DateTime lDate)
+        {
+            return false;
+        }
+
+        private JObject RequestApi()
+        {
+            string lUrlString = "https://feiertage-api.de/api/?jahr=2021&nur_land=NI";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(lUrlString);
+            WebResponse response = request.GetResponse();
+
+            using (Stream dataStream = response.GetResponseStream())
+            {
+                // Open the stream using a StreamReader for easy access.
+                StreamReader reader = new StreamReader(dataStream);
+                // Read the content.
+                string responseFromServer = reader.ReadToEnd();
+
+                JObject json = JObject.Parse(responseFromServer);
+                return json;
+
             }
         }
     }
